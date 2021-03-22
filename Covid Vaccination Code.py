6747fbde4494cef11a488cc3e86bb7c0f486ea02
pc <bailegneeve@gmail.com>
2021-03-22 19:50:17 +0000

    gg

--- Covid Vaccination Code.py
+++ Covid Vaccination Code.py
@@ -3,9 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 vac = pd.read_csv(r"/Users/pc/Downloads/country_vaccinations.csv")
+
 print(vac.head())
 print(vac.info())
 
-vac.fillna(0,inplace=True)
-vac.isnull().sum()
+vac.fillna(0, inplace=True)
+print(vac.isnull().sum())
+plt.figure(figsize=(12, 7))
+order_data = vac.groupby("country").total_vaccinations.agg("max").sort_values(ascending = False)
+order_data[order_data> 1300000].plot(kind = "bar")
+plt.ylabel("No of people vaccinated()")
+plt.title("Country")
+plt.show()
+
 
