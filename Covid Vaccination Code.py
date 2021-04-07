7d89c92104aed7c57b2505ea6ea5996cb256122c
pc <bailegneeve@gmail.com>
2021-04-07 11:41:24 +0100

    r

--- Covid Vaccination Code.py
+++ Covid Vaccination Code.py
@@ -1,16 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import numpy as np
+from pandasql import sqldf
 vac = pd.read_csv(r"/Users/pc/Downloads/country_vaccinations.csv")
+vac.to_csv("country_vaccinations.csv")
 
 print(vac.head())
 print(vac.info())
-
-vac.fillna(0, inplace=True)
+vac.fillna(value=0, inplace=True)
 print(vac.isnull().sum())
+print(sqldf("""
+select country,max(people_vaccinated_per_hundred) as reach
+from vac
+group by country 
+order by reach desc"""))
+
+
 plt.figure(figsize=(12, 7))
-order_data = vac.groupby("Vaccines").total_vaccinations.agg("max").sort_values(ascending = False)
-order_data[order_data> 1300000].plot(kind = "bar")
+order_data = vac.groupby("country").total_vaccinations_per_hundred.agg("max").sort_values(ascending = False)
+order_data[order_data> 20].plot(kind = "bar")
 plt.ylabel("No of people vaccinated()")
 plt.title("Country")
 plt.show()
