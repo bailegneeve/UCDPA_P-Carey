9b45c8e1b3490f098722509ad31265a31c40e67f
pc <bailegneeve@gmail.com>
2021-04-07 15:50:39 +0100

    r

--- Country Vaccination Code 2.py
+++ Country Vaccination Code 2.py
@@ -1,14 +1,9 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from pandasql import sqldf
+
 vac = pd.read_csv(r"country_vaccinations.csv")
-
-print(vac.head())
-print(vac.info())
 vac.fillna(value=0, inplace=True)
-print(vac.isnull().sum())
-
 
 plt.figure(figsize=(12, 7))
 order_data = vac.groupby("country").total_vaccinations.agg("max").sort_values(ascending = False)
--- Country Vaccination Code 6.py
+++ Country Vaccination Code 6.py
@@ -1,12 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.dates import DateFormatter, WeekdayLocator
-from datetime import timedelta
 
-vac = pd.read_csv(r"country_vaccinations.csv", usecols=['date', 'country', 'total_vaccinations'],
-                  parse_dates=['date'])
-countries = ['United States', 'Germany', 'United Kingdom', 'Israel', 'Ireland']
-vac = vac[vac['country'].isin(countries)]
-vac.fillna(value=0, inplace=True)
-for index in range(len(countries)):print('countries:', countries[index])
 
