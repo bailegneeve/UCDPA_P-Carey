6a0af20ff138c401c364ba5bfed39b7f844a266d
pc <bailegneeve@gmail.com>
2021-04-07 16:26:00 +0100

    r

--- Country Vaccination Code 3.py
+++ Country Vaccination Code 3.py
@@ -13,11 +13,11 @@ vac.fillna(value=0, inplace=True)
 
 cols = ['country', 'total_vaccinations', 'iso_code', 'vaccines','total_vaccinations_per_hundred']
 vacc_amount = vac[cols].groupby('country').max().sort_values('total_vaccinations_per_hundred', ascending=False).dropna(subset=['total_vaccinations_per_hundred'])
-vacc_amount = vacc_amount.iloc[:10]
+vacc_amount = vacc_amount.iloc[:9]
 
 vacc_amount = vacc_amount.sort_values('total_vaccinations_per_hundred', ascending=False)
 
-plt.figure(figsize=(9, 15))
+plt.figure(figsize=(8, 10))
 sns.barplot(vacc_amount.index, vacc_amount.total_vaccinations_per_hundred, color = 'g')
 plt.ylabel('Number of vaccinated people per hundred')
 plt.xlabel('Countries')
