035454b18f9e9d4521b4a277cbe8a14ea6d74281
pc <bailegneeve@gmail.com>
2021-04-10 21:36:09 +0100

    r

--- Covid Vaccination  Code 6.py
+++ Covid Vaccination  Code 6.py
@@ -4,14 +4,14 @@ import numpy as np
 import seaborn as sns
 vac = pd.read_csv(r"country_vaccinations.csv")
 
-plt.figure(figsize=(16,7))
+plt.figure(figsize=(15,6))
 grp = ['country', 'total_vaccinations', 'iso_code', 'vaccines']
-vacc_no = vac[grp].groupby('vaccines').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
+vacnum = vac[grp].groupby('vaccines').max().sort_values('total_vaccinations', ascending=False).dropna(subset=['total_vaccinations'])
 
 
-plt.bar(vacc_no.index, vacc_no.total_vaccinations, color ='m')
+plt.bar(vacnum.index, vacnum.total_vaccinations, color ='g')
 
-plt.title('Various categories of COVID-19 vaccines offered')
+plt.title('Categories of COVID-19 vaccines offered')
 plt.xticks(rotation = 90)
 plt.ylabel('Number vaccinated per 10 Million)')
 plt.xlabel('Vaccines')
